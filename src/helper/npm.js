76baf74d6d74f7022b569607defd5adc9b0180f5
Dejan Fajfar <dejan@fajfar.com>
2019-06-17 00:20:44 +0200

    Added loading of global modules

--- src/helper/npm.js
+++ src/helper/npm.js
@@ -1,11 +1,17 @@
 "use strict";
 
 const { spawnSync } = require( 'child_process' );
+const path = require('path');
 
 module.exports.getTemplate = templateName => {
-    return options => "";
+    module.paths.push(getNpmPath()); // push the global node folder to the search path
+    return module.require(templateName); // load module
 };
 
-module.exports.getNpmPath = () => {
-    return spawnSync('npm', ['root', '--json']).stdout;
+const getNpmPath = () => {
+    // Get the global module root folder (nvm safe)
+    return spawnSync('npm', ['root', '-g', '--json'])
+        .stdout.
+        toString().
+        replace(/(\r\n|\n|\r)/gm, ""); // remove trailing newline!
 };
