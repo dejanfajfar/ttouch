64a2f3089123d57dfd9dbe53162f30b49d4f407a
Dejan Fajfar <dejan.fajfar@admiral.at>
2019-06-24 10:47:48 +0200

    Fixed the loading of template libraries under windows

--- src/helper/npm.js
+++ src/helper/npm.js
@@ -1,22 +1,27 @@
 "use strict";
 
+const path = require('path');
+
 const TemplateNotFoundError = require('../errors/templateNotFoundError');
 
 const { spawnSync } = require( 'child_process' );
 
 module.exports.getTemplate = templateName => {
-    try {
-        module.paths.push(getNpmPath()); // push the global node folder to the search path
-        return module.require(templateName); // load module
-    } catch (e) {
-        throw new TemplateNotFoundError(templateName);
-    }
+	try {
+		module.paths.push(getNpmPath()); // push the global node folder to the search path
+		return module.require(templateName); // load module
+	} catch (e) {
+		throw new TemplateNotFoundError(templateName, e.message);
+	}
 };
 
 const getNpmPath = () => {
-    // Get the global module root folder (nvm safe)
-    return spawnSync('npm', ['root', '-g', '--json'])
-        .stdout.
-        toString().
-        replace(/(\r\n|\n|\r)/gm, ""); // remove trailing newline!
+	// Get the global module root folder (nvm safe)
+	let rootPath = spawnSync('npm', ['root', '-g'], {
+		shell: true
+	});
+
+	let rootDir = path.dirname(rootPath.stdout.toString()) // Get a OS specific path to the npm global library
+
+	return path.join(rootDir, 'node_modules');
 };
