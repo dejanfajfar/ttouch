3fa49b516f719f0710e372a599d51c8c3c13b511
Dejan Fajfar <dejan@fajfar.com>
2019-11-03 22:59:17 +0100

    Removed unused code parts

--- src/parts/resolveTemplate.js
+++ src/parts/resolveTemplate.js
@@ -3,15 +3,6 @@
 const templateResolver = require("../templates/templateResolver");
 const printer = require("../shared/printer");
 
-const lifeCycleMap = {
-	[templateResolver.LC_RESOLVE_START]: "Resolving template...",
-	[templateResolver.LC_RESOLVE_COMPLETE]: "Template resolved",
-	[templateResolver.LC_GIST_RETRIEVE]: "Retrieving GIST...",
-	[templateResolver.LC_GIST_RETRIEVED]: "Done",
-	[templateResolver.LC_FILE_START_READ]: "Reading template from the given file",
-	[templateResolver.LC_FILE_DONE_READ]: "Done"
-};
-
 /**
  * Resolves the given templates into the shared templateStore
  * @param {string[]} templateIdentifiers - A list of template identifiers for which to retrieve the template text
